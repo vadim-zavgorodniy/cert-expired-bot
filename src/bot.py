20c49f4438fcceee63d9fbef36a2a86600d5e92d
Vadim Zavgorodniy <iwarmouse@gmail.com>
2023-04-20 14:20:07 +0300

    Add logger and format code with yapf

--- src/bot.py
+++ src/bot.py
@@ -5,6 +5,9 @@ from functools import wraps
 import bot_config as conf
 from storage import CertStore, CertModel, ParseError
 
+from bot_logger import get_logger
+
+_logger = get_logger(__name__)
 _bot = telebot.TeleBot(conf.BOT_TOKEN)
 
 _ADD_NEW_CERT_TEXT = """Введите данные о сертификате в формате
@@ -19,12 +22,13 @@ def getCertStore():
 
 # ------------------------------------------------------------
 def log_error(func):
+
     @wraps(func)
     def log_error_decorator(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except Exception as e:
-            print(str(e.__class__) + ": Ошибка при работе с меню:" + str(e))
+        except Exception:
+            _logger.exception(str("Ошибка при работе с меню."))
 
     return log_error_decorator
 
@@ -34,28 +38,22 @@ def log_error(func):
 @log_error
 def start(message):
     with getCertStore() as store:
-        if message.text == '/list':
+        if message.text == "/list":
             cert_list = store.find_all_certs(message.from_user.id)
             if not len(cert_list):
-                _bot.send_message(
-                    message.from_user.id,
-                    "Вы еще не добавили ни одного сертификата.\nДля добавления введите /add"
-                )
+                _bot.send_message(message.from_user.id,
+                                  "Вы еще не добавили ни одного сертификата.\nДля добавления введите /add")
             else:
                 result = CertModel.list_to_string_list(cert_list)
                 _bot.send_message(message.from_user.id, "\n".join(result))
-        elif message.text == '/add':
+        elif message.text == "/add":
             _bot.send_message(message.from_user.id, _ADD_NEW_CERT_TEXT)
-            _bot.register_next_step_handler(
-                message, add_cert)  # следующий шаг – функция add_cert
-        elif message.text == '/del':
-            _bot.send_message(message.from_user.id,
-                              "Для удаления укажите common name сертификата.")
-            _bot.register_next_step_handler(
-                message, del_cert)  # следующий шаг – функция del_cert
+            _bot.register_next_step_handler(message, add_cert)  # следующий шаг – функция add_cert
+        elif message.text == "/del":
+            _bot.send_message(message.from_user.id, "Для удаления укажите common name сертификата.")
+            _bot.register_next_step_handler(message, del_cert)  # следующий шаг – функция del_cert
         else:
-            _bot.send_message(message.from_user.id,
-                              "Доступные команды: /list /add /del")
+            _bot.send_message(message.from_user.id, "Доступные команды: /list /add /del")
 
 
 # ------------------------------------------------------------
@@ -65,23 +63,18 @@ def add_cert(message):  # добавляем данные сертификата
     try:
         new_cert = CertModel.from_string(message.text)
     except ParseError as e:
-        _bot.send_message(message.from_user.id,
-                          "Проблема :(\n{}\n".format(e) + _ADD_NEW_CERT_TEXT)
+        _bot.send_message(message.from_user.id, "Проблема :(\n{}\n".format(e) + _ADD_NEW_CERT_TEXT)
         return
 
     with getCertStore() as store:
         rows = store.find_by_cn(message.from_user.id, new_cert.cn)
         if len(rows):
-            _bot.send_message(
-                message.from_user.id,
-                "Похоже, что такой сертификат уже учтен. Можно его удалить - /del\n"
-                + rows[0])
+            _bot.send_message(message.from_user.id,
+                              "Похоже, что такой сертификат уже учтен. Можно его удалить - /del\n" + rows[0])
         else:
             new_cert.user_id = message.from_user.id
-            # store.add_cert((message.from_user.id, new_cert.get("cn"), new_cert.get("description"), new_cert.get("exp_date")))
             store.add_cert(new_cert)
-            _bot.send_message(message.from_user.id,
-                              "Данные сохранены:\n" + message.text)
+            _bot.send_message(message.from_user.id, "Данные сохранены:\n" + message.text)
 
 
 # ------------------------------------------------------------
@@ -90,15 +83,11 @@ def del_cert(message):  # удаляем сертификат
     with getCertStore() as store:
         rows = store.find_by_cn(message.from_user.id, message.text.strip())
         if not len(rows):
-            _bot.send_message(
-                message.from_user.id,
-                "Сертификат с таким CN не найден. Можно проверить его наличие с помощью /list"
-            )
-        else:
-            # cert = CertTools.cert_to_dict(rows[0])
-            store.delete_cert(message.from_user.id, rows[0].id)
             _bot.send_message(message.from_user.id,
-                              "Данные удалены:\n" + str(rows[0]))
+                              "Сертификат с таким CN не найден. Можно проверить его наличие с помощью /list")
+        else:
+            store.delete_cert(message.from_user.id, rows[0].id)
+            _bot.send_message(message.from_user.id, "Данные удалены:\n" + str(rows[0]))
 
 
 # ------------------------------------------------------------
--- src/storage.py
+++ src/storage.py
@@ -1,11 +1,15 @@
 import sqlite3
-import time
 from sqlite3 import Error
+
+from bot_logger import get_logger
+
+_logger = get_logger(__name__)
 
 
 # ------------------------------------------------------------
 class ParseError(Exception):
     pass
+
 
 # Пример кортежа идущего в БД на вставку
 # (123456, "test.dev.domain.ru", "Тестовый сертификат", '2023-06-10')
@@ -34,8 +38,7 @@ class CertModel:
         if len(parts) != 3:
             raise ParseError("Не удалось разобрать данные: " + cert_str)
 
-        return CertModel(
-            (None, None, parts[1].strip(), parts[2].strip(), parts[0].strip()))
+        return CertModel((None, None, parts[1].strip(), parts[2].strip(), parts[0].strip()))
 
 
 # ------------------------------------------------------------
@@ -67,47 +70,43 @@ class CertStore:
     def add_cert(self, cert):
         try:
             cur = self.conn.cursor()
-            cur.execute(
-                f"insert into cert( {CertStore._INSERT_DB_FIELDS_STR} ) values(?,?,?,?);",
-                (cert.user_id, cert.cn, cert.description, cert.exp_date))
+            cur.execute(f"insert into cert( {CertStore._INSERT_DB_FIELDS_STR} ) values(?,?,?,?);",
+                        (cert.user_id, cert.cn, cert.description, cert.exp_date))
             self.conn.commit()
             cert.id = cur.lastrowid
         except Error as e:
-            print(e)
-            raise e
+            _logger.exception("Ошибка записи в БД: " + str(e))
+            raise
         return cert
 
     def delete_cert(self, user_id, cert_id):
         try:
             cur = self.conn.cursor()
-            cur.execute("delete from cert where id = ? and user_id = ?;",
-                        (cert_id, user_id))
+            cur.execute("delete from cert where id = ? and user_id = ?;", (cert_id, user_id))
             self.conn.commit()
         except Error as e:
-            print(e)
-            raise e
+            _logger.exception("Ошибка удаления записи из БД: " + str(e))
+            raise
 
     def get_cert(self, user_id, cert_id):
         try:
             cur = self.conn.cursor()
-            cur.execute(
-                "select id, user_id, cn, description, exp_date from cert where id = ? and user_id = ?;",
-                (cert_id, user_id))
+            cur.execute("select id, user_id, cn, description, exp_date from cert where id = ? and user_id = ?;",
+                        (cert_id, user_id))
             rows = cur.fetchall()
         except Error as e:
-            print(e)
-            raise e
+            _logger.exception("Ошибка получения записи из БД: " + str(e))
+            raise
         return CertStore.rows_to_cert_model(rows)
 
     def find_by_cn(self, user_id, common_name):
         try:
             cur = self.conn.cursor()
-            cur.execute(
-                "select id, user_id, cn, description, exp_date from cert where user_id = ? and cn = ?;",
-                (user_id, common_name))
+            cur.execute("select id, user_id, cn, description, exp_date from cert where user_id = ? and cn = ?;",
+                        (user_id, common_name))
             rows = cur.fetchall()
         except Error as e:
-            print(e)
+            _logger.exception("Ошибка поиска записи в БД: " + str(e))
             raise e
         return CertStore.rows_to_cert_model(rows)
 
